3060842b6c5a8363b05a19e6bf3cf1d745882ddc
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-03-10 17:56:27 +0100

    move my lazy nvim config to nvchad

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = { enable = true },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+        "vimdoc",
+
+        -- devops
+        "bash",
+        "tmux",
+        "dockerfile",
+        "helm",
+        "terraform",
+        "bicep",
+        "groovy",
+        "go",
+        "python",
+
+        -- productivity
+        "markdown",
+        "xml",
+        "yaml",
+        "regex",
+        "json",
+        "jq",
+        "sql",
+        "comment",
+        "diff",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+
+       -- low level
+        "c",
+        "zig"
+      },
+    },
+  },
+}
