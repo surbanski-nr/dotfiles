96965a1c0ddd0142ed6cef59fd6ae2f57c56ac12
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-03-11 23:11:24 +0100

    add my plugins to nvchad

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -57,4 +57,69 @@ return {
       },
     },
   },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+        "ansible-language-server",
+        "ansible-lint",
+        "bash-language-server",
+        "cfn-lint",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "groovy-language-server",
+        "helm-ls",
+        "htmlbeautifier",
+        "jq",
+        "jq-lsp",
+        "json-lsp",
+        "jsonlint",
+        "markdownlint",
+        "nginx-language-server",
+        "pylint",
+        "python-lsp-server",
+        "shellcheck",
+        "shellharden",
+        "snyk",
+        "snyk-ls",
+        "sql-formatter",
+        "sqlfluff",
+        "sqlls",
+        "terraform-ls",
+        "trivy",
+        "typescript-language-server",
+        "xmlformatter",
+        "yaml-language-server",
+        "yamlfix",
+        "yamllint",
+        "yq"
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        -- pyright = {},
+        bashls = {},
+        -- gopls = {},
+        bicep = {},
+        yamlls = {},
+        marksman = {},
+        powershell_es = {},
+        -- azure_pipelines_ls = {},
+        terraformls = {},
+        helm_ls = {},
+      },
+    },
+  },
+
 }
