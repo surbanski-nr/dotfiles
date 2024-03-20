9e24900f3871cf1e708acdfbcd3c6bc7898815b9
Szymon Urbanski <szymon.urbanski@netreveal.ai>
2024-03-20 17:00:23 +0100

    add yaml-companion and harpoon

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,4 +7,7 @@ local map = vim.keymap.set
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>q", "<CMD>q<CR>", { desc = "󰗼 Close" })
+map("n", "<leader>qq", "<<CMD>qa!<CR>", { desc = "󰗼 Exit" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
