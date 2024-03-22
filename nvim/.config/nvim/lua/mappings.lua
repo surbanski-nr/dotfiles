4b693f8d5c3a86c422e574f41764cdebb9503707
Szymon Urbanski <szymon.urbanski@netreveal.ai>
2024-03-22 12:28:35 +0100

    update harpoon keybinds

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,6 +8,7 @@ local map = vim.keymap.set
 map("i", "jk", "<ESC>")
 
 map("n", "<leader>q", "<CMD>q<CR>", { desc = "󰗼 Close" })
-map("n", "<leader>qq", "<<CMD>qa!<CR>", { desc = "󰗼 Exit" })
+map("n", "<leader>qq", "<CMD>qa!<CR>", { desc = "󰗼 Exit" })
 
+map("n", "<leader>as", "<CMD>ASToggle<CR>", { desc = "Auto-save toggle" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -4,58 +4,77 @@ return {
     branch = "harpoon2",
     event = "VeryLazy",
     opts = {},
-    config = function()
-      local harpoon = require "harpoon"
+    keys = {
+      { "<Leader>ua", "ga", desc = "Show character under cursor" },
+      {
+        "<Leader>a",
+        function()
+          require("harpoon"):list():append()
+        end,
+        desc = "Add location",
+      },
+      {
+        "<C-n>",
+        function()
+          require("harpoon"):list():next()
+        end,
+        desc = "Next location",
+      },
+      {
+        "<C-p>",
+        function()
+          require("harpoon"):list():prev()
+        end,
+        desc = "Previous location",
+      },
+      {
+        "<Leader>mr",
+        function()
+          require("harpoon"):list():remove()
+        end,
+        desc = "Remove location",
+      },
+      {
+        "<Leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Harpoon select 1",
+      },
+      {
+        "<Leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Harpoon select 2",
+      },
+      {
+        "<Leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Harpoon select 3",
+      },
+      {
+        "<Leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Harpoon select 4",
+      },
 
-      -- REQUIRED
-      harpoon:setup()
-      -- REQUIRED
-
-      local conf = require("telescope.config").values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
-
-        require("telescope.pickers")
-          .new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table {
-              results = file_paths,
-            },
-            previewer = conf.file_previewer {},
-            sorter = conf.generic_sorter {},
-          })
-          :find()
-      end
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
-      end)
-      -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-      vim.keymap.set("n", "<C-e>", function()
-        toggle_telescope(harpoon:list())
-      end, { desc = "Open harpoon window" })
-      -- vim.keymap.set("n", "<C-h>", function()
-      --   harpoon:list():select(1)
-      -- end)
-      -- vim.keymap.set("n", "<C-t>", function()
-      --   harpoon:list():select(2)
-      -- end)
-      -- vim.keymap.set("n", "<C-n>", function()
-      --   harpoon:list():select(3)
-      -- end)
-      -- vim.keymap.set("n", "<C-s>", function()
-      --   harpoon:list():select(4)
-      -- end)
-
-      -- Toggle previous & next buffers stored within Harpoon list
-      -- vim.keymap.set("n", "<C-S-P>", function()
-      --   harpoon:list():prev()
-      -- end)
-      -- vim.keymap.set("n", "<C-S-N>", function()
-      --   harpoon:list():next()
-      -- end)
-    end,
+      {
+        "<Leader>lh",
+        function()
+          local harpoon = require "harpoon"
+          if not require("lazyvim.util").has "telescope.nvim" then
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+            return
+          end
+          return require "telescope._extensions.marks"()
+        end,
+        desc = "List locations",
+      },
+    },
   },
 }
