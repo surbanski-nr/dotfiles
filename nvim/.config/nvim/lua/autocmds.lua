099dc75fec4a3595309fc8fbb96d6d53a51e91c1
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-03-30 17:14:15 +0100

    sync

---
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    require("cmp").setup { enabled = false }
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  -- group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown", "pandoc" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
+
+vim.api.nvim_create_autocmd("filetype", {
+  -- group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown", "pandoc" },
+  command = "set nospell",
+})
