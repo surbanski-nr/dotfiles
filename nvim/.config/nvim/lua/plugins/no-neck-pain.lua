ed9d282a84c9ac2ed64c4c3c5d25e9c347d4ca1e
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-04-10 21:05:38 +0200

    update plugins

---
+++ nvim/.config/nvim/lua/plugins/no-neck-pain.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "shortcuts/no-neck-pain.nvim",
+    version = "*",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>tn", "<cmd>NoNeckPain<cr>", desc = "No [N]eckpain" },
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/numb.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nacro90/numb.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("numb").setup()
+    end,
+  },
+}
