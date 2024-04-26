171a6568f03b3d7fd8112c198c2133b4cf319fb4
Szymon Urbanski <szymon.urbanski@netreveal.ai>
2024-04-26 08:39:29 +0200

    update line numbers in nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h13" }
-
+vim.opt.number = false
+vim.opt.relativenumber = true
 -- local o = vim.o
 -- o.cursorlineopt = "both" -- to enable cursorline!
