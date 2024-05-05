27a2ffc0b430b566aee7cf584acea6ee0adc6801
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-05-05 18:25:46 +0200

    sync

---
+++ nvim/.config/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,49 @@
+return {
+  autotag = {
+    enable = true,
+  },
+  ensure_installed = {
+    -- defaults
+    "vim",
+    "lua",
+    "vimdoc",
+
+    -- devops
+    "bash",
+    "tmux",
+    "dockerfile",
+    "helm",
+    "terraform",
+    "hcl",
+    "bicep",
+    "groovy",
+    "go",
+    "gomod",
+    "gowork",
+    "gosum",
+    "python",
+
+    -- productivity
+    "markdown",
+    "markdown_inline",
+    "xml",
+    "yaml",
+    "regex",
+    "json",
+    "jq",
+    "sql",
+    "comment",
+    "diff",
+
+    -- web dev
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+
+    -- low level
+    "c",
+    "zig",
+  },
+}
