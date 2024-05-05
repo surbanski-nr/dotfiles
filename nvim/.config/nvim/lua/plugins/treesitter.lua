f27aa2e7afe447116a3cf3ad9a57a6688bc8bd97
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-05-05 18:25:46 +0200

    sync

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,51 +4,6 @@ return {
     dependencies = {
       "windwp/nvim-ts-autotag",
     },
-    opts = {
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        -- defaults
-        "vim",
-        "lua",
-        "vimdoc",
-
-        -- devops
-        "bash",
-        "tmux",
-        "dockerfile",
-        "helm",
-        "terraform",
-        "hcl",
-        "bicep",
-        "groovy",
-        "go",
-        "python",
-
-        -- productivity
-        "markdown",
-        "markdown_inline",
-        "xml",
-        "yaml",
-        "regex",
-        "json",
-        "jq",
-        "sql",
-        "comment",
-        "diff",
-
-        -- web dev
-        "html",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-
-        -- low level
-        "c",
-        "zig",
-      },
-    },
+    opts = require "configs.treesitter",
   },
 }
