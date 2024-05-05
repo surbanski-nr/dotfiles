8dc10dd3cb9e65302ea21086ad198543dea8141c
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-05-05 23:16:37 +0200

    add new mappings to neovim; add precognition plugin

--- nvim/.config/nvim/lua/plugins/bqf.lua
+++ nvim/.config/nvim/lua/plugins/bqf.lua
@@ -1,3 +1,10 @@
+-- zn - new list for signed items
+-- zN - new list for unsigned items
+-- P - preview all items
+-- p - preview current item
+-- o - open and close
+-- <CR> - open
+-- <C-v> - open in vertical split
 return {
   {
     "kevinhwang91/nvim-bqf",
