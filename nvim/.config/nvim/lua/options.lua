171f4d6cb83c19a83cd0a8c71ebe4a2e82bc7d8c
Szymon Urbanski <szymon.urbanski@netreveal.ai>
2024-05-16 10:14:34 +0200

    sync

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,5 +4,7 @@ vim.opt.guifont = { "JetBrainsMono Nerd Font", ":h13" }
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- local o = vim.o
+local o = vim.o
+o.verbose = 3
+o.verbosefile = "/tmp/nvimlog"
 -- o.cursorlineopt = "both" -- to enable cursorline!
--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -6,7 +6,7 @@ return {
     event = "VeryLazy",
     config = function()
       require("chatgpt").setup {
-        api_key_cmd = "pass chatgpt/invhub-shared-oai",
+        -- api_key_cmd = "pass chatgpt/invhub-shared-oai",
         api_type_cmd = "echo azure",
         azure_api_base_cmd = "echo https://invhub-shared-oai.openai.azure.com",
         azure_api_engine_cmd = "echo gpt-4_1106-Preview",
@@ -27,13 +27,19 @@ return {
       "nvim-telescope/telescope.nvim",
     },
     keys = {
-      { "<leader>ag", mode = { "n", "v" }, "<CMD>ChatGPT<CR>", desc = "Toggle [G]PT Chat" },
+      { "<leader>ah", mode = { "n", "v" }, "<CMD>ChatGPT<CR>", desc = "Toggle [G]PT Chat" },
       { "<leader>ht", mode = { "n", "v" }, "<CMD>ChatGPTRun add_tests<CR>", desc = "ChatGPT Add [T]ests" },
       { "<leader>ho", mode = { "n", "v" }, "<CMD>ChatGPTRun optimize_code<CR>", desc = "ChatGPT [O]ptimize Code" },
       { "<leader>hs", mode = { "n", "v" }, "<CMD>ChatGPTRun summarize<CR>", desc = "ChatGPT [S]ummarize" },
       { "<leader>hf", mode = { "n", "v" }, "<CMD>ChatGPTRun fix_bugs<CR>", desc = "ChatGPT [F]ix Bugs" },
       { "<leader>hd", mode = { "n", "v" }, "<CMD>ChatGPTRun docstring<CR>", desc = "ChatGPT [D]ocstring" },
       { "<leader>hx", mode = { "n", "v" }, "<CMD>ChatGPTRun explain_code<CR>", desc = "ChatGPT E[X]plain Code" },
+      {
+        "<leader>hr",
+        mode = { "n", "v" },
+        "<CMD>ChatGPTRun code_readability_analysis<CR>",
+        desc = "ChatGPT [R]eadability Check",
+      },
       { "<leader>he", mode = { "n", "v" }, "<CMD>ChatGPTEditWithInstructions<CR>", desc = "ChatGPT [E]dit" },
     },
   },
