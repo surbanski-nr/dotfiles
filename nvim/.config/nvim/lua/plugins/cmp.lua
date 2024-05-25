905ecdfbdd029b94dcff41b4986f00322b4e2bd7
surbanski <122265380+surbanski-nr@users.noreply.github.com>
2024-05-25 21:25:23 +0200

    sync

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+
     dependencies = {
       {
         "zbirenbaum/copilot-cmp",
@@ -9,10 +10,18 @@ return {
         end,
       },
     },
+
     opts = {
-      enabled = function()
-        return (vim.bo.ft ~= "markdown")
-      end,
+      -- enabled = function()
+      --   local context = require "cmp.config.context"
+      --   local disabled = false
+      --   disabled = disabled or (vim.api.nvim_get_option_value("buftype", { buf = 0 }) == "prompt")
+      --   disabled = disabled or (vim.fn.reg_recording() ~= "")
+      --   disabled = disabled or (vim.fn.reg_executing() ~= "")
+      --   disabled = disabled or (context.in_treesitter_capture "comment" or context.in_syntax_group "Comment")
+      --   disabled = disabled or (vim.bo.ft ~= "markdown")
+      --   return not disabled
+      -- end,
 
       sources = {
         { name = "nvim_lsp" },
--- nvim/.config/nvim/lua/plugins/kustomize.lua
+++ nvim/.config/nvim/lua/plugins/kustomize.lua
@@ -1,20 +1,21 @@
 return {
-  {
-    "allaman/kustomize.nvim",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-    },
-    ft = "yaml",
-    opts = { enable_key_mappings = false },
-    config = function(opts)
-      require("kustomize").setup { opts }
-      -- default keybindings, adjust to your needs
-      vim.keymap.set("n", "<leader>kb", "<cmd>lua require('kustomize').build()<cr>", { noremap = true })
-      vim.keymap.set("n", "<leader>kk", "<cmd>lua require('kustomize').kinds()<cr>", { noremap = true })
-      vim.keymap.set("n", "<leader>kl", "<cmd>lua require('kustomize').list_resources()<cr>", { noremap = true })
-      vim.keymap.set("n", "<leader>kp", "<cmd>lua require('kustomize').print_resources()<cr>", { noremap = true })
-      vim.keymap.set("n", "<leader>kv", "<cmd>lua require('kustomize').validate()<cr>", { noremap = true })
-      vim.keymap.set("n", "<leader>kd", "<cmd>lua require('kustomize').deprecations()<cr>", { noremap = true })
-    end,
-  },
+  -- {
+  --   "allaman/kustomize.nvim",
+  --   dependencies = {
+  --     { "nvim-lua/plenary.nvim" },
+  --   },
+  --   enable = false,
+  --   ft = "yaml",
+  --   opts = { enable_key_mappings = false },
+  --   config = function(opts)
+  --     require("kustomize").setup { opts }
+  --     -- default keybindings, adjust to your needs
+  --     vim.keymap.set("n", "<leader>kb", "<cmd>lua require('kustomize').build()<cr>", { noremap = true })
+  --     vim.keymap.set("n", "<leader>kk", "<cmd>lua require('kustomize').kinds()<cr>", { noremap = true })
+  --     vim.keymap.set("n", "<leader>kl", "<cmd>lua require('kustomize').list_resources()<cr>", { noremap = true })
+  --     vim.keymap.set("n", "<leader>kp", "<cmd>lua require('kustomize').print_resources()<cr>", { noremap = true })
+  --     vim.keymap.set("n", "<leader>kv", "<cmd>lua require('kustomize').validate()<cr>", { noremap = true })
+  --     vim.keymap.set("n", "<leader>kd", "<cmd>lua require('kustomize').deprecations()<cr>", { noremap = true })
+  --   end,
+  -- },
 }
