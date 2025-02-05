f9a04c9b6bd83f4fa00a5d3a5008035febff9656
Szymon Urbanski <szymon.urbanski@netreveal.ai>
2025-02-05 15:10:37 +0100

    add option to disable auto formatting on save

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -21,11 +21,13 @@ local options = {
     terraform = { "terraform_fmt" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
 }
 
 return options
