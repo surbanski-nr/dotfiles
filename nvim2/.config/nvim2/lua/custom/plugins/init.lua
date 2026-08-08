58a076ea83318a8db09c18778af886d70fd53ef5
Szymon Urbański <122265380+surbanski-nr@users.noreply.github.com>
2026-08-08 10:29:49 +0000

    nvim2: disable session persistence

--- nvim2/.config/nvim2/lua/custom/plugins/init.lua
+++ nvim2/.config/nvim2/lua/custom/plugins/init.lua
@@ -1,7 +1,3 @@
-local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
-for file_name, entry_type in vim.fs.dir(plugins_dir, { follow = true }) do
-  if (entry_type == 'file' or entry_type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
-    local module = file_name:gsub('%.lua$', '')
-    require('custom.plugins.' .. module)
-  end
-end
+require 'custom.plugins.highlight_colors'
+-- require 'custom.plugins.persistence'
+require 'custom.plugins.render_markdown'
